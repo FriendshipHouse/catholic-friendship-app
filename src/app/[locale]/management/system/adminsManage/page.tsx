a6961d9f047469d469ea2fdd26cc8507cdeb6853
catholicfriendship <catholicfriendshiphouse@gmail.com>
2024-12-12 14:17:46 +0800

    feat: created admins page

---
+++ src/app/[locale]/management/system/adminsManage/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import AdminsFormTable from '@/components/management/admins/AdminsFormTable';
+
+function AdminsManage() {
+  return (
+    <div className="p-5">
+      <AdminsFormTable />
+    </div>
+  );
+}
+
+export default AdminsManage;

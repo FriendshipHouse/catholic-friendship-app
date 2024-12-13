d371114527ec2af402c68273c59c5ddbae6d6d9e
catholicfriendship <catholicfriendshiphouse@gmail.com>
2024-12-13 13:21:38 +0800

    feat: created member page

---
+++ src/app/[locale]/house/member/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import MemberCard from '@/components/house/member/MemberCard';
+
+import avatarCai from '@/../public/about/avatars/img-member-cai.png';
+import avatarMa from '@/../public/about/avatars/img-member-ma.png';
+import avatarQiu from '@/../public/about/avatars/img-member-qiu.png';
+import avatarSu from '@/../public/about/avatars/img-member-su.png';
+import avatarTang from '@/../public/about/avatars/img-member-tang.png';
+import avatarWang from '@/../public/about/avatars/img-member-wang.png';
+import avatarYeh from '@/../public/about/avatars/img-member-yeh.png';
+import avatarYou from '@/../public/about/avatars/img-member-you.png';
+
+function Members() {
+  const memberList = [
+    { value: 'ma', position: '指導神師', name: '馬智明 神父', avatarSrc: avatarMa },
+    { value: 'tang', position: '牧靈工作者', name: '唐曉倫 小姐', avatarSrc: avatarTang },
+    { value: 'cai', position: '牧靈工作者', name: '蔡宜蓁 小姐', avatarSrc: avatarCai },
+    { value: 'su', position: '牧靈工作者', name: '蘇美紗 小姐', avatarSrc: avatarSu },
+    { value: 'qiu', position: '牧靈工作者', name: '邱加利 老師', avatarSrc: avatarQiu },
+    { value: 'wang', position: '牧靈工作者', name: '王沛琪 小姐', avatarSrc: avatarWang },
+    { value: 'you', position: '語言老師', name: '游惠茹 老師', avatarSrc: avatarYou },
+    { value: 'yeh', position: '語言老師', name: '葉秋雨 老師', avatarSrc: avatarYeh },
+  ];
+
+  return (
+    <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+      {memberList.map(({ value, position, name, avatarSrc }) => (
+        <MemberCard key={value} position={position} name={name} avatarSrc={avatarSrc} />
+      ))}
+    </div>
+  );
+}
+
+export default Members;
---
+++ src/components/house/member/MemberCard.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import Image, { StaticImageData } from 'next/image';
+
+type MemberCardType = {
+  avatarSrc: StaticImageData;
+  position: string;
+  name: string;
+};
+
+function MemberCard({ avatarSrc, position, name }: Readonly<MemberCardType>) {
+  return (
+    <div className="flex flex-col justify-center gap-4 rounded-md border border-gray-40 bg-white p-4 shadow-4xl">
+      <div className="mx-auto h-auto w-auto">
+        <Image src={avatarSrc} alt="avarImage" className="h-full w-full rounded-md" />
+      </div>
+      <div className="flex flex-col gap-2 text-center text-[16px]">
+        <div>{position}</div>
+        <div>{name}</div>
+      </div>
+    </div>
+  );
+}
+
+export default MemberCard;

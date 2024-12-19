10a8e1f9ee787db6e187374195b813190ca3813e
catholicfriendship <catholicfriendshiphouse@gmail.com>
2024-12-19 15:47:19 +0800

    feat: login requirement of user page

--- src/app/[locale]/user/layout.tsx
+++ src/app/[locale]/user/layout.tsx
@@ -2,17 +2,22 @@
 
 import { useMemo } from 'react';
 
+import { Button } from 'antd';
+import { useSession } from 'next-auth/react';
 import { useTranslations } from 'next-intl';
 
 import TitleHighlight from '@/components/TitleHighlight';
 import Background from '@/components/layout/Background';
 import Footer from '@/components/layout/Footer';
 
-import { usePathname } from '@/i18n/routing';
+import { Link, usePathname } from '@/i18n/routing';
 
 function AboutLayout({ children }: Readonly<RootProvider>) {
   const pathname = usePathname();
-  const t = useTranslations('account');
+  const t = useTranslations();
+
+  const { status } = useSession();
+  const isLogin = status === 'authenticated';
 
   const title = useMemo<string | null>(() => {
     if (!pathname || pathname === '/') return null;
@@ -23,19 +28,26 @@ function AboutLayout({ children }: Readonly<RootProvider>) {
     return pageName;
   }, [pathname]);
 
-  return (
+  return isLogin ? (
     <div className="flex w-full flex-col">
       <Background />
       <div className="relative z-10 mx-auto flex h-full w-full max-w-screen-lg flex-col justify-between bg-white shadow-4xl">
         <div className="flex h-full flex-col">
           <div className="flex flex-col gap-10 px-5 pt-5 md:px-10 md:pt-10">
-            <TitleHighlight>{t(title)}</TitleHighlight>
+            <TitleHighlight>{t(`account.${title}`)}</TitleHighlight>
             <div className="w-full border-b border-primary-30" />
           </div>
           <div className="h-full p-5 md:p-10">{children}</div>
         </div>
         <Footer />
       </div>
+    </div>
+  ) : (
+    <div className="flex w-full justify-center items-center flex-col gap-6 text-xl font-bold">
+      {t('management.permissionDeny')}
+      <Link href="/">
+        <Button>{t('management.backHome')}</Button>
+      </Link>
     </div>
   );
 }

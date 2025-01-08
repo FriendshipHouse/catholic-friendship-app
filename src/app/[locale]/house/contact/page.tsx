8cd61e5e22e6b789e681764b618cc9d1bb569d9a
catholicfriendship <catholicfriendshiphouse@gmail.com>
2025-01-08 12:01:32 +0800

    feat: updated contact information

--- src/app/[locale]/house/contact/page.tsx
+++ src/app/[locale]/house/contact/page.tsx
@@ -1,8 +1,27 @@
 'use client';
 
+import { Button } from 'antd';
 import React from 'react';
 
-import { House, TruckFront } from 'react-bootstrap-icons';
+import { GeoAlt, House, TruckFront } from 'react-bootstrap-icons';
+
+const carParking = [
+  { parkingLot: "1/ 博客停車場-家福同安場(總車位 17)", mapHref: "https://maps.app.goo.gl/XoDfuwo9a4bhmwtr9" },
+  { parkingLot: "2/ 嘟嘟房廈門站停車場(總車位 7)", mapHref: "https://maps.app.goo.gl/FRh5JcrprEoRavFn9" },
+  { parkingLot: "3/ 同安街平面停車場(總車位 21)", mapHref: "https://maps.app.goo.gl/qBw4xFKjM75XKoQG6" },
+]
+
+const carContent =
+  <div className="flex flex-col gap-1">
+    <div>同安街天主堂庭院可停車(數量有限)</div>
+    <div>附近停車場：</div>
+    {carParking.map(({ parkingLot, mapHref }) => <div className="flex gap-2 items-center
+    " key={parkingLot}>
+      <Button type="text" href={mapHref} className='!text-sm !p-0 !text-primary-70'><GeoAlt /></Button>
+      {parkingLot}
+    </div>)}
+  </div>
+
 
 const contactList = [
   {
@@ -19,11 +38,11 @@ const contactList = [
       },
       {
         title: '地址',
-        content: '台北市中山北路一段2號八樓800室',
+        content: '台北市中正區同安街72巷19號(同安街聖若瑟天主堂3F)',
       },
       {
         title: '彌撒時間',
-        content: '每周六晚上7:15\n每周日晚上7:30（周日因疫情暫停）',
+        content: '每周六晚上7:00',
       },
     ],
   },
@@ -32,17 +51,17 @@ const contactList = [
     icon: <TruckFront />,
     children: [
       {
-        title: '火車捷運',
+        title: '捷運',
         content:
-          '台北車站捷運站M7出口步行約1分鐘\n（從淡水與板橋來搭第1車廂；從新店與南港來搭最後一個車廂）',
+          '台北車站-轉捷運至古亭站，2號出口步行約10分鐘',
       },
       {
         title: '公車',
-        content: '5, 262, 247, 307, 310, 227, 224, 39',
+        content: '648. 644. 251. 藍28. 252. 297. 至捷運古亭站(羅斯福)',
       },
       {
         title: '開車',
-        content: '平日晚上及週末有地下停車場可停車(數量有限)',
+        content: carContent,
       },
     ],
   },
@@ -68,7 +87,7 @@ function Contact() {
                   key={title}
                   className="flex flex-col gap-2 whitespace-pre-wrap  leading-6 pt-2"
                 >
-                  <div className="flex items-start gap-4">
+                  <div className="flex items-center gap-4">
                     <div className="w-8 min-w-8 text-wrap font-bold">{title}</div>
                     <div className="text-gray-60">{content}</div>
                   </div>
@@ -83,7 +102,7 @@ function Contact() {
         <iframe
           title="google map"
           className="h-full w-full rounded-md brightness-110 saturate-0"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.6466861661465!2d121.51691897607895!3d25.046061637804378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a972222f6c45%3A0x5f0264d335e281c2!2z5aSp5Li75pWZ5Y-L6Kq85pWZ5Y-L5rS75YuV5Lit5b-D!5e0!3m2!1sen!2stw!4v1718700097036!5m2!1sen!2stw"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3615.337063703309!2d121.51677157607848!3d25.02263283874789!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a996c8d54e79%3A0x29aae25c4a2addc3!2sTong&#39;an%20St%20St.Joseph&#39;s%20Catholic%20Church!5e0!3m2!1sen!2stw!4v1736305647171!5m2!1sen!2stw"
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
         />

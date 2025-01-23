e1e73a25bb65717eb824c8b8c5433a4a443216ee
catholicfriendship <catholicfriendshiphouse@gmail.com>
2025-01-23 13:30:42 +0800

    style: index button style fine tune

--- src/components/index/IndexButton.tsx
+++ src/components/index/IndexButton.tsx
@@ -21,7 +21,7 @@ function IndexButton({ children, pathname }: Readonly<ButtonProps>) {
       className="!group !w-full !bg-gray-20/60 !py-5 !text-gray-80 !shadow-4xl !backdrop-blur !duration-150 hover:!bg-gray-10 sm:!max-w-44 md:!max-w-52"
       onClick={onClick}
     >
-      <div className="flex items-center justify-center gap-4 text-[16px] font-semibold md:text-lg text-wrap">
+      <div className="flex items-center justify-center gap-4 text-[16px] font-semibold md:text-lg text-wrap leading-tight">
         {children}
         <ArrowRightShort className="text-2xl hover:text-primary" />
       </div>

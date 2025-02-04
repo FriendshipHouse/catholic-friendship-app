512800fadaf63f03c03f8adc5cff3b6a381d9111
catholicfriendship <catholicfriendshiphouse@gmail.com>
2025-02-04 13:09:39 +0800

    feat: google analytics revised for google search console detected

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,47 +1,60 @@
 'use client';
 
-import Script from 'next/script';
-import { usePathname, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
+
+import { usePathname, useSearchParams } from 'next/navigation';
+import Script from 'next/script';
 
 const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
 
 const GoogleAnalytics = () => {
-    const pathname = usePathname();
-    const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
-    useEffect(() => {
-        if (!GA_TRACKING_ID) return;
+  useEffect(() => {
+    if (!GA_TRACKING_ID) return;
 
-        const handleRouteChange = () => {
-            let queryString = '';
-            if (searchParams) {
-                queryString = '?' + searchParams.toString();
-            }
+    const handleRouteChange = () => {
+      let queryString = '';
+      if (searchParams) {
+        queryString = '?' + searchParams.toString();
+      }
 
-            const url = pathname + queryString;
-            window.gtag('config', GA_TRACKING_ID, { page_path: url });
-        };
+      const url = pathname + queryString;
+      window.gtag('config', GA_TRACKING_ID, { page_path: url });
+    };
 
-        handleRouteChange();
-    }, [pathname, searchParams]);
+    handleRouteChange();
+  }, [pathname, searchParams]);
 
-    return (
-        <>
-            <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-                strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-                {`
+  return (
+    <>
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
                   window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
                   gtag('config', '${GA_TRACKING_ID}');
+                `,
+        }}
+      />
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_TRACKING_ID}', {send_page_view: false});
                 `}
-            </Script>
-        </>
-    );
+      </Script>
+    </>
+  );
 };
 
 export default GoogleAnalytics;

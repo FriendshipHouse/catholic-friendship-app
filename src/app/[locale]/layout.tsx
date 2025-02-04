47041467052ba07da425f422a41cfa600a001b34
catholicfriendship <catholicfriendshiphouse@gmail.com>
2025-02-04 12:33:27 +0800

    feat: moved google analytics to <head> for google search console

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,9 +1,9 @@
 import { getTranslations } from 'next-intl/server';
 
+import GoogleAnalytics from '@/components/GoogleAnalytics';
 import PageLayout from '@/components/layout/PageLayout';
 import ClientProvider from '@/components/providers/ClientProvider';
 import ServerProvider from '@/components/providers/ServerProvider';
-import GoogleAnalytics from '@/components/GoogleAnalytics';
 
 type LocaleLayoutProps = RootProvider & {
   params: { locale: Locale };
@@ -21,10 +21,12 @@ export async function generateMetadata({ params: { locale } }: LocaleLayoutProps
 export default async function LocaleLayout({ children, params: { locale } }: LocaleLayoutProps) {
   return (
     <html lang={locale}>
+      <head>
+        <GoogleAnalytics />
+      </head>
       <body>
         <ServerProvider locale={locale}>
           <ClientProvider>
-            <GoogleAnalytics />
             <PageLayout>{children}</PageLayout>
           </ClientProvider>
         </ServerProvider>

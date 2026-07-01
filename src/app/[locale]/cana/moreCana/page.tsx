28e407b29c0115bd67305570d98f29d86814f9fc
FriendshipHouse <catholicfriendshiphouse@gmail.com>
2026-07-01 11:08:13 +0800

    feat: add moreCana page

---
+++ src/app/[locale]/cana/moreCana/page.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+import { useTranslations } from 'next-intl';
+
+import PieChart from '@/components/house/moreCana/PieChart';
+
+import imageDonatePie1 from '@/../public/about/img-donate-pie1.png';
+import imageDonatePie2 from '@/../public/about/img-donate-pie2.png';
+import imageDonatePie3 from '@/../public/about/img-donate-pie3.png';
+import imageDonatePie4 from '@/../public/about/img-donate-pie4.png';
+
+const pieList = [
+  {
+    value: '2',
+    title1: 'nonReligionFamily',
+    title2: 'participation',
+    pieSrc: imageDonatePie2,
+    description: 'description2',
+  },
+  {
+    value: '4',
+    title1: 'nonReligionFamily',
+    title2: 'under14',
+    pieSrc: imageDonatePie4,
+    description: 'description4',
+  },
+  {
+    value: '1',
+    title1: 'religionFamily',
+    title2: 'participation',
+    pieSrc: imageDonatePie1,
+    description: 'description1',
+  },
+  {
+    value: '3',
+    title1: 'religionFamily',
+    title2: 'under14',
+    pieSrc: imageDonatePie3,
+    description: 'description3',
+  },
+];
+
+function MoreCana() {
+  const t = useTranslations('moreCana');
+
+  const projectPlans = t.raw('project_plans') as { title: string; content: string }[];
+
+  return (
+    <div className="flex w-full flex-col gap-6">
+      <div className="flex flex-col gap-6 whitespace-pre-wrap leading-6">
+        <p className="">{t('introduction')}</p>
+
+        <div className="mx-auto my-4 grid w-full grid-cols-2 justify-items-center gap-x-8 gap-y-10 sm:grid-cols-4">
+          {pieList.map(({ value, title1, title2, pieSrc, description }) => (
+            <PieChart
+              key={value}
+              value={value}
+              title1={title1}
+              title2={title2}
+              pieSrc={pieSrc}
+              description={description}
+            />
+          ))}
+        </div>
+
+        <p>{t('statistics_description')}</p>
+
+        <p>{t('project_title')}</p>
+
+        <div className="flex flex-col gap-4">
+          {projectPlans.map((plan) => (
+            <div key={plan.title}>
+              <p className="font-bold">{plan.title}</p>
+              <p>{plan.content}</p>
+            </div>
+          ))}
+        </div>
+
+        <div>
+          <p className="font-bold">{t('naming_origin.title')}</p>
+          <p>{t('naming_origin.content')}</p>
+        </div>
+
+        <div>
+          <p className="font-bold">{t('transition.title')}</p>
+          <p>{t('transition.content')}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MoreCana;
---
+++ src/components/house/moreCana/PieChart.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useTranslations } from 'next-intl';
+import Image, { StaticImageData } from 'next/image';
+
+type PieChartProps = {
+  value: string;
+  title1: string;
+  title2: string;
+  pieSrc: StaticImageData;
+  description: string;
+};
+
+function PieChart({ value, title1, title2, pieSrc, description }: Readonly<PieChartProps>) {
+  const t = useTranslations('moreCana.pie');
+  return (
+    <div key={value} className="flex w-30 flex-col items-center gap-4">
+      <div className="flex flex-col gap-1 text-center font-bold">
+        <div>{t(`${title1}`)}</div>
+        <div>{t(`${title2}`)}</div>
+      </div>
+      <div className="w-20 sm:w-24">
+        <Image src={pieSrc} alt="pieChart_image" loading="lazy" />
+      </div>
+      <div className="text-center">{t(`${description}`)}</div>
+    </div>
+  );
+}
+
+export default PieChart;
